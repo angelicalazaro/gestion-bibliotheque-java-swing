0481fee9b21778475261b2bf8a32c3fdd9d756b7
Angelica Lazaro <angelicamld@gmail.com>
2025-11-04 16:58:13 +0100

    class book finished

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Bibliotheque {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+// ActionListener exige actionPerformed(ActionEvent e)
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//ItemListener exige itemStateChanged(ItemEvent e)
+public class Bibliotheque extends JFrame implements ActionListener, ItemListener{
+    // attributs
+    // Panels
+    private JPanel p1;
+    private JPanel p2;
+    private JPanel p3;
+    private JPanel p4;
+    private JPanel p5;
+    private JPanel p6;
+    private JPanel p7;
+    private JPanel p8;
+    private JPanel p9;
+    // labels
+    private JLabel LCodeLivre;
+    private JLabel LTitreLivre;
+    private JLabel LAuteur;
+    private JLabel LTotLiv;
+    private JLabel LLivDispo;
+    private JLabel LCodeLiv;
+    // text field
+    private JTextField TFcodeLivre;
+    private JTextField TFtitreLivre;
+    private JTextField TFauteur;
+    private JTextField TFtotLiv;
+    private JTextField TFlivDispo;
+    // buttons
+    private JButton BConsulter;
+    private JButton BEmprunter;
+    private JButton BRendre;
+    private JButton BEditer;
+    private JButton BAjouter;
+    private JButton BSupprimer;
+    private JButton BQuitter;
+    // radio buttons
+    private JRadioButton RBtous;
+    private JRadioButton RBempruntes;
+    // checkbox - combo
+    private JCheckBox
+    private ArrayList<Livre> LIV;
+
+    // constructeur
+    public Bibliotheque(){
+        LIV = new ArrayList<>();
+        p1 = new JPanel();
+        LCodeLivre = new JLabel("Code livre : ");
+        TFcodeLivre = new JTextField("", 5);
+        BConsulter = new JButton("Consulter");
+        p1.add(LCodeLivre);
+        p1.add(TFcodeLivre);
+        p1.add(BConsulter);
+        p1.setBackground(new Color(145,146,207));
+    }
+    public static void main(String[] args)
+    {
+        Bibliotheque MyBib = new Bibliotheque();
+        MyBib.setVisible(true);
     }
 }
--- src/Livre.java
+++ src/Livre.java
@@ -1,2 +1,44 @@
 public class Livre {
+    private int code;
+    private String titre;
+    private String auteur;
+    private int totalLivres;
+    private int dispoLivres;
+
+    public Livre (int code, String titre, String auteur, int totalLivres, int dispoLivres){
+        this.code = code;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.totalLivres = totalLivres;
+        this.dispoLivres = dispoLivres;
+    }
+
+    public int getCode(){return code;}
+    public String getTitre(){return titre;}
+    public String getAuteur(){return auteur;}
+    public int getTotalLivres(){return totalLivres;}
+    public int getDispoLivres(){return dispoLivres;}
+
+    public void setCode(int code){this.code = code;}
+    public void setTitre(String titre){this.titre = titre;}
+    public void setAuteur(String auteur){this.auteur = auteur;}
+    public void setTotalLivres(int totalLivres){this.totalLivres = totalLivres;}
+    public void setDispoLivres(int dispoLivres){this.dispoLivres = dispoLivres;}
+    // creer une liste des livres ?
+    public boolean emprunter() {
+        if(this.dispoLivres > 0) {
+            this.dispoLivres--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean rendre() {
+        if(dispoLivres < totalLivres) {
+            dispoLivres++;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
